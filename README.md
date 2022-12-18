a05aa22a91bf638403dc75752b271ef3d574c81c
Mattia Ceschin <33133311+cesxhin@users.noreply.github.com>
2022-12-18 09:01:10 +0100

    Add project

---
+++ README.md
@@ -0,0 +1,27 @@
+## 💾Update Service
+Questo progetto verrà utilizzato per controllare se sono presenti nel file locale se non ci sono, invia un messaggio a DownloadService che scarica l'episodio mancante.
+### Information general:
+- `require` volume mounted on Docker
+### Variabili globali richiesti:
+```sh
+example:
+    #--- Rabbit ---
+    USERNAME_RABBIT: "guest" #guest [default]
+    PASSWORD_RABBIT: "guest" #guest [default]
+    ADDRESS_RABBIT: "localhost" #localhost [default]
+    
+    #--- API ---
+    ADDRESS_API: "localhost" #localhost [default]
+    PORT_API: "33333" #5000 [default]
+    PROTOCOL_API: "http" or "https" #http [default]
+    
+    #--- Logger ---
+    LOG_LEVEL: "Debug|Info|Error" #Info [default]
+    WEBHOOK_DISCORD_DEBUG: "url" [not require]
+    
+    #--- General ---
+    BASE_PATH: "/folder/anime" or "D:\\\\Directory\Anime" #/ [default]
+    TIME_REFRESH: "60000" <-- milliseconds #120000 [default] 2 minutes
+    LIMIT_THREAD_PARALLEL: "8" #5 [default]
+    SELECT_SERVICE: "manga or anime" #anime
+```
---
+++ src/Cesxhin.AnimeManga.UpdateService/Worker.cs
@@ -0,0 +1,44 @@
+using Cesxhin.AnimeManga.Application.CheckManager.Interfaces;
+using Cesxhin.AnimeManga.Application.NlogManager;
+using Microsoft.Extensions.Hosting;
+using NLog;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cesxhin.AnimeManga.UpdateService
+{
+    public class Worker : BackgroundService
+    {
+        //log
+        private readonly NLogConsole _logger = new(LogManager.GetCurrentClassLogger());
+
+        //timer
+        private readonly int _timeRefresh = int.Parse(Environment.GetEnvironmentVariable("TIME_REFRESH") ?? "120000");
+
+        //service
+        private readonly IUpdate _update;
+
+        public Worker(IUpdate update)
+        {
+            _update = update;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    _update.ExecuteUpdate();
+                }catch (Exception ex)
+                {
+                    _logger.Fatal($"Error update, details error: {ex}");
+                }
+
+                _logger.Info($"Worker running at: {DateTimeOffset.Now}");
+                await Task.Delay(_timeRefresh, stoppingToken);
+            }
+        }
+    }
+}
